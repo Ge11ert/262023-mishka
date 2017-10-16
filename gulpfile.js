d37e6766e97a1489d11beeb2b97414d746a42e5a
Ge11ert <gellert94@mail.ru>
2017-10-16 18:16:09 +0300

    Добавлены пакеты и задачи gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,17 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var posthtml = require("gulp-posthtml");
+var includeHtml = require("posthtml-include");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -16,13 +24,62 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+    return gulp.src("img/**/*.{png,jpg,svg}")
+        .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.jpegtran({progressive: true}),
+          imagemin.svgo()
+        ]))
+        .pipe(gulp.dest("img/optimized"));
 });
 
 gulp.task("webp", function () {
     return gulp.src("img/**/*.{jpg,png}")
         .pipe(webp({quality: 85}))
         .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function() {
+    return gulp.src("img/icon-*.svg")
+        .pipe(svgstore({
+          inlineSvg: true
+        }))
+        .pipe(rename("sprite-auto.svg"))
+        .pipe(gulp.dest("img"));
+});
+
+gulp.task("html", function() {
+    return gulp.src("*.html")
+        .pipe(posthtml([
+          includeHtml()
+        ]))
+        .pipe(gulp.dest("."))
+});
+
+gulp.task("clean", function() {
+    return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+    run("style", "sprite", "html", done);
 });
 
 gulp.task("serve", ["style"], function() {
